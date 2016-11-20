cd93dab275b5cdde8cb8fadf8895e6c8c55ea1cf
EC2 Default User <ec2-user@ip-172-31-18-152.us-west-2.compute.internal>
2016-11-20 22:13:24 +0000

    Added install.sh file for convinience

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+sudo pip install Django==1.9.2
+
+sudo pip install wheel==0.29.0
+
+sudo pip install certifi
+
+sudo pip install elasticsearch
+
+sudo pip install watson-developer-cloud
+
